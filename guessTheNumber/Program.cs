59428f3a7fc4f2f7caf951b27d2ebbea9e324568
Nikgmz <niki.ouzunov@gmail.om>
2024-09-18 13:08:47 +0300

    Code finished

--- guessTheNumber/Program.cs
+++ guessTheNumber/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+int computerNumber = random.Next(0, 101);
+try
+{
+    while (true)
+    {
+        Console.Write("Enter anumber between 0 and 100: ");
+        int playerNumber = int.Parse(Console.ReadLine());
+        if(playerNumber == computerNumber)
+        {
+            Console.WriteLine("Correct!");
+            break;
+        }
+        else if(playerNumber > computerNumber)
+        {
+            Console.WriteLine("Too high!");
+        }
+        else
+        {
+            Console.WriteLine("Too low!");
+        }
+    }
+}
+catch
+{
+    Console.WriteLine("Invalid input!");
+}
